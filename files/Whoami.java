0c29b8c6c1f80968c0ede6a559f47725c014a1f4
Bas Meijer <bas.meijer@me.com>
2017-05-30 21:28:47 +0200

    j2 refactor

--- files/Whoami.java
+++ files/Whoami.java
@@ -1,25 +1,21 @@
 import java.io.*;
 
-
 public class Whoami {
   public static void main(String[] args) {
-    String userName = System.getProperty("user.name");
-    String userDir = System.getProperty("user.dir");
-    String userHome = System.getProperty("user.home");
     String osName = System.getProperty("os.name");
     String osArch = System.getProperty("os.arch");
     String osVersion = System.getProperty("os.version");
-    System.out.println("{ \n");
-    System.out.println("  \"changed\":" + " false,");
-    System.out.println("  \"rc\":" + " 0,");
-    System.out.println("  \"stderr_lines\":" + " [],");
-    System.out.println("  \"results\":" + " {");
-    System.out.println("     \"os.name\": \"" + osName + "\" ," );
-    System.out.println("     \"os.arch\": \"" + osArch + "\" ," );
-    System.out.println("     \"os.version\": \"" + osVersion + "\" ," );
-    System.out.println("     \"user.dir\": \"" + userDir + "\" ," );
-    System.out.println("     \"user.home\": \"" + userHome + "\" ," );
-    System.out.println("     \"user.name\": \"" + userName + "\" " );
-    System.out.println("  }\n}");
+
+    String userName = System.getProperty("user.name");
+    String userDir = System.getProperty("user.dir");
+    String userHome = System.getProperty("user.home");
+
+    System.out.println("os.name:" + osName );
+    System.out.println("os.arch:" + osArch );
+    System.out.println("os.version:" + osVersion );
+
+    System.out.println("user.name:" + userName );
+    System.out.println("user.dir:" + userDir );
+    System.out.println("user.home:" + userHome );
   }
 }
