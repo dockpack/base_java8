ee198740c22b1793a8fd8d7db1b887ad0cd32047
Bas Meijer <bas.meijer@me.com>
2022-03-31 16:38:42 +0200

    Fixes bug in PATH, caused Java-11 not to be used when selected.

--- templates/java.sh
+++ templates/java.sh
@@ -1,5 +1,4 @@
-# /base_java8/templates/jre.sh
+# {{ ansible_managed }}
 export JRE_HOME={{ java8_home }}/jre
 export JAVA_HOME={{ java8_home }}
 export JDK_HOME=$JAVA_HOME
-export PATH=$PATH:$JAVA_HOME/bin
